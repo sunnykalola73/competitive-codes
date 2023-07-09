911ed8021f5cd32318a5322c7b5de62de842161b
Sunny Kalola <sunnykalola73@gmail.com>
2023-07-09 09:24:09 -0400

    largest_cont_sum_Kenade

--- CPP/largest_cont_sum_Kenade.cpp
+++ CPP/largest_cont_sum_Kenade.cpp
@@ -30,16 +30,28 @@ int main()
 }
 void solve(int a[], int size)
 {
-	int max_so_far = INT_MIN, max_end_here =0;
+	int max_so_far = INT_MIN, max_end_here =0, start=0, end=0, s=0;
 
 	for(int i=0;i<size;i++){
 		max_end_here += a[i];
-		max_so_far = max(max_so_far, max_end_here);
 
-		if(max_end_here < 0)
+		if (max_so_far < max_end_here) {
+            max_so_far = max_end_here;
+            start = s;
+            end = i;
+        }
+
+		if(max_end_here < 0){
+
+
 			max_end_here =0;
+			s = i+1;
+		}
 
 	} 
 
 	cout<<max_so_far<<endl;
+
+	// This prints the start adn end index of the subarray
+	cout<<start<<"-"<<end<<endl;
 }
